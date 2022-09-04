4184ab355f9424a9fb190974f3d810a4579108ff
Hatim Piplodwala <hpiplodwala110@gmail.com>
2022-09-04 14:11:15 -0500

    recovered old project3

--- Project-3/mail/mail/static/mail/inbox.js
+++ Project-3/mail/mail/static/mail/inbox.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#compose').addEventListener('click', compose_email);
 
   // By default, load the inbox
-  load_mailbox('inbox');
+  load_mailbox('inboxfdfdfdfdf');
 });
 
 function compose_email() {
@@ -20,7 +20,44 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+  document.querySelector('#compose-form').onsubmit = () => {
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+          recipients: document.querySelector('#compose-recipients').value,
+          subject: document.querySelector('#compose-subject').value,
+          body: document.querySelector('#compose-body').value
+      })
+    })
+    .then(response => {
+      clone = response.clone();
+      if (response.status === 400 || response.status === 404){
+        clone.json()
+        .then(error => compose_error(error))
+      }
+      else{
+        localStorage.clear();
+        load_mailbox('inbox');
+      }
+    })
+
+    return false;
+  }
 }
+
+function compose_error(error){
+  const errorblock = document.querySelector('#compose-error');
+  errorblock.replaceChildren();
+  const element = document.createElement('div');
+  element.classList.add("alert", "alert-danger");
+  element.innerHTML = `${error.error}`;
+  errorblock.append(element);
+  document.querySelector('#compose-recipients').value = '';
+  document.querySelector('#compose-subject').value = '';
+  document.querySelector('#compose-body').value = '';
+  document.body.scrollTop = document.documentElement.scrollTop = 0;
+}
+
 
 function load_mailbox(mailbox) {
   
@@ -31,8 +68,31 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
   fetch(`/emails/${mailbox}`)
-  .then(response => response.json())
-  .then(emails => {
-    
-  })
+  .then(response => {
+    clone = response.clone();
+    if (response.status === 400 || response.status === 404){
+      clone.json()
+      .then(error => {
+        const errorblock = document.querySelector('#emails-view');
+        const element = document.createElement('div');
+        element.classList.add("alert", "alert-danger");
+        element.innerHTML = `${error.error}`;
+        errorblock.append(element);
+        document.body.scrollTop = document.documentElement.scrollTop = 0;
+      })
+    }
+    else{
+      clone.json()
+      .then(emails => emails.forEach(email => {
+        const element=document.createElement('div');
+        if (email.read){
+          element.className = "readbox";
+        }
+        else{
+          element.className = "unreadbox";
+        }
+        element.innerHTML = `${email.subject} ${email.body} ${email.timestamp}`;
+        document.querySelector("#emails-view").append(element);
+      }))
+  }})
 }
